479959da17c34d7d62aa0f0dad69e169c0ca1200
hilolaar <aripovah14@gmail.com>
2021-12-04 18:32:49 +0600

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstNum = 2;
+let second_num = 3;
+let ThirdNum = 1;
+
+alert(firstNum);
+alert(second_num);
+alert(ThirdNum);
+
+/*let  str1='САЛАМ, ';
+let str2 = 'БИШКЕК!';
+let result = (str1 + str2);
+
+console.log(result);*/
+
+let name = "Хилола!";
+let name1 = "Привет,"
+alert(name1 + name );
+
+let age = 15;
+let str2 = "Мне ";
+let str3 = " Возраст ";
+let str4 = " лет!";
+
+alert(str2 + age +str3+ age +str4);
